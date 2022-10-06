3a6be8527bad98133af318871bd5b9a2aaecc0b7
aasgher385 <asgher.ali@lums.edu.pk>
2022-10-06 12:26:20 +0500

    Updated Changes to environment

--- Bisection.jl
+++ Bisection.jl
@@ -16,12 +16,16 @@
 
 # Import Packages
 import Pkg
-Pkg.add("Printf")
+Pkg.add("Plots")
 Pkg.add("Roots")
+Pkg.add("Printf")
+Pkg.add("LaTeXStrings")
 
 # Load Packages
 using Printf
+using Plots
 using Roots
+using LaTeXStrings
 
 # Define Function to be Rooted
 # We considered f(x) = x²-2, x ∈ [0,2] with the solution x = 1.4142135623730951
@@ -35,6 +39,11 @@ b = 2.0
 
 # Bisection Method 
 function bisection(f,a,b)
+
+    @printf("__________________________________________________\n \n");
+    @printf("k        a         b         Root        Error    \n");
+    @printf("__________________________________________________\n \n");
+
     
     if f(a)*f(b)>0 
         println("Failed")
@@ -42,8 +51,7 @@ function bisection(f,a,b)
         p = (a + b)/2;
         i = 0 
         err = abs(f(p));
-        display("Its    a       b            Root          Error")
-        while err > 1e-8
+            while err > 1e-8
             i += 1
             if f(a)*f(p)<0 
                 b = p;
@@ -53,11 +61,20 @@ function bisection(f,a,b)
             p = (a + b)/2; 
             err = abs(f(p));
             @printf("%1.0f %1.9f %1.9f %1.9f %1.9f \n", i, a, b, p, err)
-        end 
+        end
+        return p 
     end
 end 
 
 
 res = bisection(f,0,2)
 
-find_zero(f, (0,2), Bisection())
+k1 = find_zero(f, (0,2), Bisection())
+
+x = range(0,stop=2,length=1000)
+plot(x,f.(x),linewidth=3.0,xlabel=L"x",label=L"f(x)", legend=:topleft)
+plot!([res],[0], markershape=:circle,markercolor=:red, markerstrokecolor=:black, markerstrokewidth=3,
+	label= "Root",markersize=10)
+plot!([k1],[0], markershape=:star,markercolor=:blue, markerstrokecolor=:black, markerstrokewidth=3,
+	label= "Root by using Bisection Method from Roots.jl",markersize=6)
+
